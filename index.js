671358d9ef568fd1b19cdda98106fe876b2bf709
Ricardo de la Vega Barrón <rdelavega63@gmail.com>
2018-02-19 10:06:51 -0600

    reprocess db

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ transferdb.on("child_changed", function(snapshot) {
 
 });
 
-transferdb.orderByKey().equalTo("-L59aXtMubMc0wNeYH2w").on("child_added", function(snapshot) {
+transferdb.on("child_added", function(snapshot) {
 
   positiondb.orderByKey().equalTo(snapshot.val().key_encuesta)
     .limitToLast(1).once("value").then(function(snap) {
